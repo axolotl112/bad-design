2f685bf9d28220aff3f01fb490c787cbca5a5578
divine structure <40558734+axolotl112@users.noreply.github.com>
2022-11-28 04:08:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+    //slider
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+     
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+    
+      
+      
+      });
